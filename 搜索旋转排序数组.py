8584733f1803a15eb0b444c9df892276bb8b8be7
holysll <holysll@163.com>
2019-04-29 23:36:11 +0800

    Update 搜索旋转排序数组.py

--- 搜索旋转排序数组.py
+++ 搜索旋转排序数组.py
@@ -13,7 +13,7 @@
 # 输入: nums = [4,5,6,7,0,1,2], target = 3
 # 输出: -1
 
-# Solution Code
+# Solution Code1
 class Solution(object):
     def core(self,a,i,j,t):
         if i>j:
@@ -55,3 +55,40 @@ class Solution(object):
         if len(nums)==0:
             return -1
         return self.core(nums,0,len(nums)-1,target)
+
+    
+# Solution Code2    
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid
+                else:
+                    high = mid - 1
+
+        return -1
+
+if __name__ == "__main__":
+    nums = [4,5,6,7,0,1,2]
+    target = 0
+    print(Solution().search(nums, target))
