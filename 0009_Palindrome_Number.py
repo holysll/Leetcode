324feb5bebfbba549b8901b1c8e0322946b909d5
holysll <holysll@163.com>
2019-07-23 14:26:56 +0800

    回文数

--- 0009_Palindrome_Number.py
+++ 0009_Palindrome_Number.py
@@ -19,7 +19,7 @@
 # 进阶:
 # 你能不将整数转为字符串来解决这个问题吗？
 
-# Solution code
+# Solution code1(转换为字符串，翻转)
 
 class Solution(object):
     def isPalindrome(self, x):
@@ -28,8 +28,30 @@ class Solution(object):
         :rtype: bool
         """
         return str(x) == str(x)[::-1]
-        
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/palindrome-number
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    
+# Solution code2(通过% 和 //判断):
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # 特殊情况：
+        # 如上所述，当 x < 0 时，x 不是回文数。
+        # 同样地，如果数字的最后一位是 0，为了使该数字为回文，则其第一位数字也应该是 0，只有 0 满足这一属性
+        if x < 0 or (x !=0 and x % 10 == 0):
+            return False
+        y = 0
+        while x > y:
+            y = 10 * y + x % 10
+            x //= 10
+        # 当数字长度为奇数时，我们可以通过 y//10 去除处于中位的数字。
+        # 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，y = 123，
+        # 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == y or x == y // 10
+    
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/palindrome-number
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
