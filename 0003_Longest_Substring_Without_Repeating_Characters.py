9e6e6c775a41f907437286388c6a2fb288597b30
holysll <holysll@sina.com>
2020-08-01 17:47:22 +0800

    无重复字符的最长子串

--- 0003_Longest_Substring_Without_Repeating_Characters.py
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -1,25 +1,37 @@
-# 无重复字符的最长子串
-# 题目: 
-#    给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# some description
+# author: holysll
+# datetime: 2020-5-8 01:47
+# software: PyCharm
+"""
+题目：无重复字符的最长子串
 
-# 示例 1:
-#   输入: "abcabcbb"
-#   输出: 3 
-#   解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
-# 示例 2:
-#   输入: "bbbbb"
-#   输出: 1
-#   解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 1:
 
-# 示例 3:
-#   输入: "pwwkew"
-#   输出: 3
-#   解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
 
-# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
 
-# Solution Code
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# 双指针+哈希表
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -27,10 +39,59 @@ class Solution(object):
         :rtype: int
         """
         st = {}
-        i, ans = 0, 0
+        i, ans = 0, 0  # 这里i的下标从0开始
         for j in range(len(s)):
             if s[j] in st:
                 i = max(st[s[j]], i)
             ans = max(ans, j - i + 1)
             st[s[j]] = j + 1
-        return ans;
+        return ans
+
+
+# 动态规划 + 哈希表
+class Solution1(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        res = tmp = 0
+        for j in range(len(s)):
+            i = dic.get(s[j], -1)  # 获取索引 i，如果没有，默认为-1
+            dic[s[j]] = j  # 更新哈希表
+            tmp = tmp + 1 if tmp < j - i else j - i
+            res = max(res, tmp)
+        return res
+
+
+class Solution2(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        start = res = 0
+        for k, v in enumerate(s):
+            if v in dic and start <= dic[v]:
+                start = dic[v] + 1
+            else:
+                res = max(res, k - start + 1)
+            dic[v] = k
+        return res
+
+
+if __name__ == "__main__":
+    s = "pwwkew"
+    solution = Solution()
+    res = solution.lengthOfLongestSubstring(s)
+    print(res)
+
+    solution1 = Solution1()
+    res1 = solution1.lengthOfLongestSubstring(s)
+    print(res1)
+
+    solution2 = Solution2()
+    res2 = solution2.lengthOfLongestSubstring(s)
+    print(res2)
