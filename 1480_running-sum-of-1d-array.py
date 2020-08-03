0a1388d4c7190a21640a2823565e0f7d1b729678
holysll <holysll@sina.com>
2020-08-03 16:31:02 +0800

    一维数组的动态和

---
+++ 1480_running-sum-of-1d-array.py
@@ -0,0 +1,75 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# some description
+# author: holysll
+# datetime: 2020-8-3 15:47
+# software: PyCharm
+"""
+题目：一维数组的动态和
+
+给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+
+请返回 nums 的动态和。
+
+示例 1：
+
+输入：nums = [1,2,3,4]
+输出：[1,3,6,10]
+解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。
+示例 2：
+
+输入：nums = [1,1,1,1,1]
+输出：[1,2,3,4,5]
+解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+示例 3：
+
+输入：nums = [3,1,2,10,1]
+输出：[3,4,6,16,17]
+
+提示：
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/running-sum-of-1d-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Python packages
+
+# 遍历，动态规划
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i - 1]
+        return nums
+
+
+# 利用额外的列表存结果
+class Solution1(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        sum = 0
+        for num in nums:
+            sum = sum + num
+            res.append(sum)
+        return res
+
+
+if __name__ == '__main__':
+    nums = [3, 1, 2, 10, 1]
+    solution = Solution()
+    res = solution.runningSum(nums)
+    print(res)
